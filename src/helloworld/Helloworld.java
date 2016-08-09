a4cc2e8e5ca4c47193f3ccdb9f0595803f97900e
shonenki@gmail.com <shonenki@gmail.com>
2016-08-08 23:04:01 -0400

    HelloWorld de Java V 1.0

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,14 @@
+package helloworld;
+/**
+ * @author shonenki
+ * @fecha 08/08/2016
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        //instancio la clase testJava en la variable obj
+        testJava obj = new testJava();
+        //llamo al método público "ejecutar" de la clase testJava
+        obj.ejecutar();
+    }
+    
+}
